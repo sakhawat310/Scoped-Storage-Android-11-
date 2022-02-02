7e367c80aa5ae453a09a854489b00ffce337e0f2
sakhawat310 <98877118+sakhawat310@users.noreply.github.com>
2022-02-02 15:18:06 +0600

    Create getExternalStorageDir() method

--- getExternalStorageDir.java
+++ getExternalStorageDir.java
@@ -4,6 +4,7 @@ public class Initialization{
      *
      * @param context required if sdk greater than {@link Build.VERSION_CODES#Q}, otherwise can be null
      * @return absolute path of external storage directory.
+     * type is the file type
      */
     public static String getExternalStorageDir(Context context, String type){
         String externalRootDir;
